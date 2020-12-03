3c329a6d69458457d9e75f4533d81b1ad0050b66
User <anatienko.yv@gmail.com>
2020-12-03 21:59:54 +0300

    hibernate

---
+++ src/main/java/jdbc/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,101 @@
+package jdbc.dao;
+
+import jdbc.model.User;
+import jdbc.util.Util;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class UserDaoHibernateImpl implements UserDao {
+    private SessionFactory factory;
+
+    private String createUserTable = "CREATE TABLE public.coreusers\n" +
+            "    (id bigserial NOT NULL,\n" +
+            "    name character varying,\n" +
+            "    last_name character varying,\n" +
+            "    age integer,\n" +
+            "    PRIMARY KEY (id))";
+
+    private String dropUserTable = "drop table public.coreusers";
+
+    private String insertUser = "INSERT INTO public.coreusers \n" +
+            "(name, last_name, age)\n" +
+            "VALUES (:name, :last_name, :age)";
+
+    private String removeUser = "DELETE from public.coreusers WHERE id = :id";
+
+    private String getAllUser = "select * from public.coreusers";
+
+    private String clearUser = "delete from public.coreusers";
+
+    public UserDaoHibernateImpl() {
+        factory = Util.getHibernateFactory();
+    }
+
+
+    @Override
+    public void createUsersTable() {
+        executeQuery(createUserTable);
+    }
+
+    @Override
+    public void dropUsersTable() {
+        executeQuery(dropUserTable);
+    }
+
+    private void executeQuery(String dropUserTable) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.createSQLQuery(dropUserTable).executeUpdate();
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+        transaction.commit();
+        session.close();
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createSQLQuery(insertUser);
+        query.setParameter("name", name);
+        query.setParameter("last_name", lastName);
+        query.setParameter("age", age);
+        query.executeUpdate();
+        transaction.commit();
+        session.close();
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createSQLQuery(removeUser);
+        query.setParameter("id", id);
+        query.executeUpdate();
+        transaction.commit();
+        session.close();
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> result;
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createSQLQuery(getAllUser).addEntity(User.class);
+        result = query.list();
+        transaction.commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public void cleanUsersTable() {
+        executeQuery(clearUser);
+    }
+}
---
+++ src/main/java/jdbc/util/Util.java
@@ -0,0 +1,47 @@
+package jdbc.util;
+
+import jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import org.hibernate.metamodel.Metadata;
+import org.hibernate.metamodel.MetadataSources;
+import org.hibernate.service.ServiceRegistry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Util {
+    private static String driver = "org.postgresql.Driver";
+    private static String database = "coretask";
+    private static String user = "postgres";
+    private static String password = "postgres";
+
+    public static Connection getJDBCConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(driver);
+        return DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/" + database, user, password);
+    }
+
+    public static SessionFactory getHibernateFactory(){
+        Map<String, String> settings = new HashMap<>();
+        settings.put(Environment.DRIVER, driver);
+        settings.put(Environment.URL, "jdbc:postgresql://127.0.0.1:5432/" + database);
+        settings.put(Environment.USER, user);
+        settings.put(Environment.PASS, password);
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        settings.put(Environment.SHOW_SQL, "true");
+        settings.put(Environment.HBM2DDL_AUTO, "update");
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(settings).build();
+
+        MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+        metadataSources.addAnnotatedClass(User.class);
+        Metadata metadata = metadataSources.buildMetadata();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        return sessionFactory;
+    }
+}
